896c2e9cf9d4e84ed6fca6f427b3143b0275f1a6
brunrs1 <raph.bruno@hotmail.com>
2022-01-15 16:33:58 -0300

    commit inicial

---
+++ aula05/tevenada.js
@@ -0,0 +1,3 @@
+/*só teve coisas sobre variaveis 
+ensinando de uma forma mais teorica, como as variaveis são feitas com var e let, como elas ocupam espaços em memorias, e como elas podem ser mudadas depois, acredito que mais pra frente ele vai falar sobre a const que é uma variavel que não pode ser alterada, ele também ensinou sobre o typeof, que vai mostrar qual vai ser o tipe da variavel, e tem alguns como string, number, null, booleam, undefined e por ai va.
+não teve nenhum exercicio, mas da para praticar a soma entre as variaveis e a criação de variaveis contendo informações pessoais */
---
+++ aula07/valores.js
@@ -0,0 +1,9 @@
+5 > 2 
+7 < 4 
+8 >= 8
+9 <= 7
+5 == 5   //dois sinais de iguais é igual um só é recebe
+4 != 4    // != quer dizer é diferente
+5 == '5' // javascript reconhece pois mesmo que o 5 seja string ele ai é 5
+5 === '5'   // geral false pois 3 = significa identico 
+// ! quer dizer negação       && conjução      || disjunção
